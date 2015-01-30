c28a8b388c4716483255a95ff6e0c36a9b5404ed
Bruno Santos <brunonascimentosantos@gmail.com>
2015-01-30 16:54:33 -0200

    custom toolbar

--- app/src/main/java/com/bnsantos/navigationdrawer/activity/MainActivity.java
+++ app/src/main/java/com/bnsantos/navigationdrawer/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,12 +30,30 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        // set a custom shadow that overlays the main content when the drawer opens
-        //mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.hideOverflowMenu();
+        toolbar.findViewById(R.id.toolbarNotification).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mDrawerLayout!=null&&!mDrawerLayout.isDrawerOpen(Gravity.END)){
+                    mDrawerLayout.closeDrawer(Gravity.START);
+                    mDrawerLayout.openDrawer(Gravity.END);
+                }
+            }
+        });
+        toolbar.findViewById(R.id.toolbarLogo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mDrawerLayout!=null&&!mDrawerLayout.isDrawerOpen(Gravity.START)){
+                    mDrawerLayout.closeDrawer(Gravity.END);
+                    mDrawerLayout.openDrawer(Gravity.START);
+                }
+            }
+        });
+
         setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_drawer);
+
 
         // ActionBarDrawerToggle ties together the the proper interactions
         // between the sliding drawer and the action bar app icon
@@ -62,28 +81,6 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.content_frame, mCenter.get())
                     .commit();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_notification) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void initFragments(){
